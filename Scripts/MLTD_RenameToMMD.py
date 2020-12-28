8904255bf5fb5bb8b40172d5dbd69f3370dd9d18
Rama Alifiandy <thehoodieguy2@gmail.com>
2020-12-28 22:32:01 +0700

    Update MLTD_RenameToMMD.py

--- Scripts/MLTD_RenameToMMD.py
+++ Scripts/MLTD_RenameToMMD.py
@@ -1,5 +1,9 @@
 # Rename Million Live Theater Days armatures to MMD compatible!
 
+import bpy
+context = bpy.context
+obj = context.object
+
 # NOTE:
 #  This user script is meant to use in conjunction with MMD Tools and base MMD armature,
 #  quickly renaming MLTD bones to MMD compatible armature. Please note that this script
@@ -22,3 +26,81 @@
 #  MLTD models do not use bones for the iris position - the game relies on shifting the
 #  eye texture instead. If you want to use iris bones , try separating the iris texture
 #  from the sclera.
+
+
+
+# This array is the template used for the batch rename script.
+# The format for the array element goes like this:
+#           ('old name','new name')
+# Apostrophe is not omitted. Add comma if there's a new bone you want to rename.
+bonenames = [
+    ('BASE',                    'ParentNode'),
+    ('MUNE1',                   'UpperBody'),
+    ('MUNE2',                   'UpperBody2'),
+    ('KUBI',                    'Neck'),
+    ('ATAMA',                   'Head'),
+    ('SAKOTSU_L',               'Shoulder_L'),
+    ('KATA_L',                  'Arm_L'),
+    ('KATA_MD_L__twist_x50',    'ArmTwistAxis_L'),
+    ('KATA_RT_L__twist_x0',     'ArmTwist_L'),
+    ('UDE_L',                   'Elbow_L'),
+    ('TEKUBI_L__rot_TE_L__x100','HandTwistAxis_L'),
+    ('UDE_MD_L__rot_TE_L__x50', 'HandTwist_L'),
+    ('TE_L',                    'Wrist_L'),
+#   ('OYA3_L',                  'Thumb1_L'), # Thumb palm is often omitted from MMD armature
+    ('OYA2_L',                  'Thumb1_L'), # Increment this index if you're including thumb palm
+    ('OYA1_L',                  'Thumb2_L'), # Increment this index if you're including thumb palm
+    ('HITO3_L',                 'IndexFinger1_L'),
+    ('HITO2_L',                 'IndexFinger2_L'),
+    ('HITO1_L',                 'IndexFinger3_L'),
+    ('NAKA3_L',                 'MiddleFinger1_L'),
+    ('NAKA2_L',                 'MiddleFinger2_L'),
+    ('NAKA1_L',                 'MiddleFinger3_L'),
+    ('KUSU3_L',                 'RingFinger1_L'),
+    ('KUSU2_L',                 'RingFinger2_L'),
+    ('KUSU1_L',                 'RingFinger3_L'),
+    ('KO3_L',                   'LittleFinger1_L'),
+    ('KO2_L',                   'LittleFinger2_L'),
+    ('KO1_L',                   'LittleFinger3_L'),
+    ('SAKOTSU_R',               'Shoulder_R'),
+    ('KATA_R',                  'Arm_R'),
+    ('KATA_MD_R__twist_x50',    'ArmTwistAxis_R'),
+    ('KATA_RT_R__twist_x0',     'ArmTwist_R'),
+    ('UDE_R',                   'Elbow_R'),
+    ('TEKUBI_R__rot_TE_R__x100','HandTwistAxis_R'),
+    ('UDE_MD_R__rot_TE_R__x50', 'HandTwist_R'),
+    ('TE_R',                    'Wrist_R'),
+#   ('OYA3_R',                  'Thumb1_R'), # Thumb palm is often omitted from MMD armature
+    ('OYA2_R',                  'Thumb1_R'), # Increment this index if you're including thumb palm
+    ('OYA1_R',                  'Thumb2_R'), # Increment this index if you're including thumb palm
+    ('HITO3_R',                 'IndexFinger1_R'),
+    ('HITO2_R',                 'IndexFinger2_R'),
+    ('HITO1_R',                 'IndexFinger3_R'),
+    ('NAKA3_R',                 'MiddleFinger1_R'),
+    ('NAKA2_R',                 'MiddleFinger2_R'),
+    ('NAKA1_R',                 'MiddleFinger3_R'),
+    ('KUSU3_R',                 'RingFinger1_R'),
+    ('KUSU2_R',                 'RingFinger2_R'),
+    ('KUSU1_R',                 'RingFinger3_R'),
+    ('KO3_R',                   'LittleFinger1_R'),
+    ('KO2_R',                   'LittleFinger2_R'),
+    ('KO1_R',                   'LittleFinger3_R'),
+    ('KOSHI',                   'LowerBody'),
+    ('MOMO_L',                  'Leg_L'),
+    ('HIZA_L',                  'Knee_L'),
+    ('ASHI_L',                  'Ankle_L'),
+    ('TSUMASAKI_L'              'Toe_L'),
+    ('MOMO_R',                  'Leg_R'),
+    ('HIZA_R',                  'Knee_R'),
+    ('ASHI_R',                  'Ankle_R'),
+    ('TSUMASAKI_R'              'Toe_R'),
+]
+
+for oldname, newname in bonenames:
+    # Get the pose bone with name
+    pb = obj.pose.bones.get(oldname)
+    # Move on if no matching bone found
+    if pb is None:
+        continue
+    # rename
+    pb.name = newname
